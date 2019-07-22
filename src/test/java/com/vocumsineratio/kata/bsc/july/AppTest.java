ae16fb712377b548e9f8efb86d1278fa6626c97f
Danil <danil@vast.com>
2019-07-22 19:35:15 -0400

    Mon Jul 22 19:35:15 EDT 2019

--- src/test/java/com/vocumsineratio/kata/bsc/july/AppTest.java
+++ src/test/java/com/vocumsineratio/kata/bsc/july/AppTest.java
@@ -3,6 +3,8 @@ package com.vocumsineratio.kata.bsc.july;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 /**
  * Unit test for simple App.
@@ -12,5 +14,10 @@ public class AppTest
     @Test
     public void prove_the_test_works() {
         assertEquals(1, App.member);
+    }
+
+    @Test
+    public void uncompressed_source_is_unchanged() {
+        assertEquals("a", "a");
     }
 }
