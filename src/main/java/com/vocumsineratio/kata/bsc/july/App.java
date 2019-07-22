a533fc8303c5330c65b04d14c967ccf99415d43d
Danil <danil@vast.com>
2019-07-22 19:14:29 -0400

    Initial commit

---
+++ src/main/java/com/vocumsineratio/kata/bsc/july/App.java
@@ -0,0 +1,10 @@
+package com.vocumsineratio.kata.bsc.july;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int member = 1;
+}
---
+++ src/test/java/com/vocumsineratio/kata/bsc/july/AppTest.java
@@ -0,0 +1,16 @@
+package com.vocumsineratio.kata.bsc.july;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void prove_the_test_works() {
+        assertEquals(1, App.member);
+    }
+}
